fa2096d3df5ad9b2b7479bd3ee43f795221b8c5f
kigesui <kigesui@hotmail.com>
2015-05-05 20:47:25 -0400

    using gl2 instead of gl

--- src/com/kigesui/view/MyJoglCanvas.java
+++ src/com/kigesui/view/MyJoglCanvas.java
@@ -2,11 +2,12 @@ package com.kigesui.view;
 
 import org.apache.log4j.Logger;
 
-import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.FPSAnimator;
 
 public class MyJoglCanvas extends GLCanvas implements GLEventListener {
@@ -17,32 +18,38 @@ public class MyJoglCanvas extends GLCanvas implements GLEventListener {
 
     private static final int fps = 60;
 
+    /** The GL unit (helper class). */
+    private GLU glu;
+
     /** The OpenGL animator. */
     private FPSAnimator animator;
 
     public MyJoglCanvas(GLCapabilities capabilities, int width, int height) {
         super(capabilities);
         setSize(width, height);
+        addGLEventListener(this);
     }
 
     @Override
     public void init(GLAutoDrawable drawable) {
 //        drawable.setGL(new DebugGL(drawable.getGL()));
-        final GL gl = drawable.getGL();
+        final GL2 gl = drawable.getGL().getGL2();
 
         // Enable z- (depth) buffer for hidden surface removal. 
-        gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glDepthFunc(GL.GL_LEQUAL);
+        gl.glEnable(GL2.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL2.GL_LEQUAL);
 
         // Enable smooth shading.
-//        gl.glShadeModel(GL.GL_SMOOTH);
+        gl.glShadeModel(GL2.GL_SMOOTH);
 
         // Define "clear" color.
         gl.glClearColor(0f, 0f, 0f, 0f);
 
         // We want a nice perspective.
-//        gl.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
-        gl.glHint(GL.GL_LINE_SMOOTH_HINT, GL.GL_NICEST);
+        gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);
+
+        // Create GLU.
+        glu = new GLU();
 
         // Start animator.
         animator = new FPSAnimator(this, fps);
@@ -51,7 +58,7 @@ public class MyJoglCanvas extends GLCanvas implements GLEventListener {
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        final GL gl = drawable.getGL();
+        final GL2 gl = drawable.getGL().getGL2();
         gl.glViewport(0, 0, width, height);
     }
 
@@ -64,10 +71,21 @@ public class MyJoglCanvas extends GLCanvas implements GLEventListener {
         if (!animator.isAnimating()) {
             return;
         }
-        final GL gl = drawable.getGL();
+        final GL2 gl = drawable.getGL().getGL2();
 
         // Clear screen.
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+
+        // Set camera.
+        setCamera(gl, glu, 100);
+
+        // Write triangle.
+        gl.glColor3f(0.9f, 0.5f, 0.2f);
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+        gl.glVertex3f(-20, -20, 0);
+        gl.glVertex3f(+20, -20, 0);
+        gl.glVertex3f(0, 20, 0);
+        gl.glEnd();
     }
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
@@ -79,7 +97,21 @@ public class MyJoglCanvas extends GLCanvas implements GLEventListener {
         logger.info("dispose");
     }
 
-    
+    private void setCamera(GL2 gl, GLU glu, float distance) {
+        // Change to projection matrix.
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+
+        // Perspective.
+        float widthHeightRatio = (float) getWidth() / (float) getHeight();
+        glu.gluPerspective(45, widthHeightRatio, 1, 1000);
+        glu.gluLookAt(0, 0, distance, 0, 0, 0, 0, 1, 0);
+
+        // Change back to model view matrix.
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
     
 }
 
